45ef61439dc3e8ba8db5d67cc707194f0e530615
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-15 08:22:45 +0530

    frontend folder/file setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='text-center'>
+        <h1 className='text-5xl bg-red-100 p-2 m-2'>Hello this is App.jsx page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
