d89983deab71155a60829565cc77142eee5c946b
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-17 06:15:21 +0530

    clear

--- backend/routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-import { registerUser } from "../controllers/authController.js";
-import express from "express";
-
-const router = express.Router();
-
-router.post("/register", registerUser)
-
-export default router;
