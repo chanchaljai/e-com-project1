8c5149bf730bb8b6ccdd40ee7e1baca65a916365
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-17 11:04:45 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ProductDetails from './pages/ProductDetails'
+
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/product/:id",
+    element: <ProductDetails />,  
+  },
+]);
+
+export default function App() {
+  return (
+    <RouterProvider router={route} />
+  )
+}
