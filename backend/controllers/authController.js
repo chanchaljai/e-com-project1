a3b7be0182fec143413787f6b5679eb28d8e3ecc
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-18 10:57:15 +0530

    login test

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
@@ -29,3 +30,39 @@ export const register = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 }
+
+export const login = async (req, res) => {
+    try {
+            const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ error: "User does not exist" });
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,{
+        expiresIn: "7d",
+    });
+    // Send response
+    res.json({message: "Login successful", token, user:
+        {
+            id: user._id,
+            name: user.name,
+            email: user.email
+        }
+     });
+
+
+    }catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+    
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,8 +1,9 @@
-import { register } from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
